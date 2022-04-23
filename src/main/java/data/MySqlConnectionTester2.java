49dc437166893c3d388286ee769673f9ecb76417
ulrikkenystrup <s215849@dtu.dk>
2022-04-23 15:43:35 +0200

    Ny SQL query - slet data

--- src/main/java/data/MySqlConnectionTester2.java
+++ src/main/java/data/MySqlConnectionTester2.java
@@ -21,19 +21,34 @@ public class MySqlConnectionTester2 {
             System.out.println("Type in auth_id");
             String auth_id = scanner.next();
 
-            //opret preparedStatement og udfør SQL query
+            //opret preparedStatement og udfør SQL query, indsæt data
             String sql = "INSERT INTO doctor (first_name,sur_name, auth_id) VALUES(?, ?, ?)";
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setString(1, first_name);
             preparedStatement.setString(2, sur_name);
             preparedStatement.setString(3, auth_id);
-            preparedStatement.executeUpdate(); //anvender executeUpdate() metoden, fordi vi manipulerer data (indsætter ny data)
-            System.out.println("Successfully Inserted");  //udskriv bedsked til konsollen
+            int rowsInserted = preparedStatement.executeUpdate(); //anvender executeUpdate() metoden, fordi vi manipulerer data (indsætter ny data)
+            if (rowsInserted > 0) {
+                System.out.println("Successfully Inserted");  //udskriv bedsked til konsollen, hvis der er indsat ny data
+            }
 
-            //ny SQL query
+            //ny SQL query - slet data
+            System.out.println("Type in auth_id for the doctor to be deleted from table:"); //udskriv besked til konsollen
+            String auth_id2 = scanner.next(); //opret String objekt og tildel værdien fra det indtastede
+
+            String sql2 = "DELETE FROM doctor WHERE auth_id = ?";
+            PreparedStatement preparedStatement2 = connection.prepareStatement(sql2);
+            preparedStatement2.setString(1, auth_id2);
+
+            int rowsDeleted = preparedStatement2.executeUpdate();
+            if (rowsDeleted > 0) {
+                System.out.println("A doctor was deleted successfully!");
+            }
+
+            //ny SQL query, vis data i et resultSet
             ResultSet resultSet = preparedStatement.executeQuery("SELECT * FROM doctor"); //anvender executeQuery fordi vi vil have et resultSet udskrevet (vise data fra databasen)
 
-            //gennemløber resultSet
+            //gennemløber dvs. itererer over resultSet
             while(resultSet.next()){
                 System.out.println(resultSet.getString(1)+"  " + resultSet.getString(2)+" "+ resultSet.getInt(3));
             }
